28a1304010430bc53c431043e939a1f2a0921f1e
nikmlk <nikmlk87@gmail.com>
2021-06-04 21:25:18 +0300

    1 opt 3 HW0 opt2

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -10,7 +10,10 @@ public class MealTo {
 
     private final int calories;
 
-    private final boolean excess;
+    private final AtomicBoolean excess;      // filteredByAtomic (or any ref type, e.g. boolean[1])
+//    private final Boolean excess;            // filteredByReflection
+//    private final Supplier<Boolean> excess;  // filteredByClosure
+//    private boolean excess;
 
     public MealTo(LocalDateTime dateTime, String description, int calories, AtomicBoolean excess) {
         this.dateTime = dateTime;
@@ -18,6 +21,16 @@ public class MealTo {
         this.calories = calories;
         this.excess = excess;
     }
+
+//    for filteredByClosure
+//    public Boolean getExcess() {
+//        return excess.get();
+//    }
+
+    // for filteredBySetterRecursion
+//    public void setExcess(boolean excess) {
+//        this.excess = excess;
+//    }
 
     @Override
     public String toString() {
